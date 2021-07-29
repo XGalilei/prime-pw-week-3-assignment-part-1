7ac0b175f2a42b2ff83b213034c25b14885db938
XGalilei <ianbush1@gmail.com>
2021-07-29 10:38:22 -0500

    Continued progress

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -12,7 +12,7 @@ let foodArray = ['apple', 'chocolate'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar
 //      to the example above
-console.log('Some of my favorite foods are: ${foodArray}');
+console.log('Some of my favorite foods are: ', foodArray);
 
 
 
@@ -23,7 +23,7 @@ console.log('--- 2. Length of an array ---');
 console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
-
+console.log(foodArray.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
@@ -32,10 +32,10 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array
-
+console.log(`Second animal is ${animalArray[1]}`);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
-
+console.log('Last animal is ');
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
